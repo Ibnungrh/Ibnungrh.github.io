b8fb003313ee45cdb1ced26a2f4fbb9b30004834
Ibnungrh <ibnungrh24@gmail.com>
2024-01-09 11:27:03 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// Ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hambuger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+  if (!hambuger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
